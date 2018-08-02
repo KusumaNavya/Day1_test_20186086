b3791ef78c170c54a5c208925495269e2f95faf2
KusumaNavya <navya.kusuma21@msitprogram.net>
2018-08-02 18:10:43 +0530

    " -> 5 of 5 passed. pylint: 10.00/10 "

---
+++ p1/Assignment-1/Solution_vowels.py
@@ -0,0 +1,13 @@
+#Assume s is a string of lower case characters.
+#Number of vowels: 5
+"""number of vowels"""
+def main():
+    """find number of vowels"""
+    s_v = input("Enter a string:")
+    c_v = 0
+    for char in s_v:
+        if char in "AEIOUaeiou":
+            c_v += 1
+    print("Number of vowels are:"+str(c_v))
+if __name__ == "__main__":
+    main()
