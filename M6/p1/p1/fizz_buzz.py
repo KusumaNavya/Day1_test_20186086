0bcdb51d80b1858bf7c2151ad84546cce2146ab9
KusumaNavya <navya.kusuma21@msitprogram.net>
2018-08-04 11:18:06 +0530

    " -> 2 of 7 passed. pylint: 10.00/10 "

--- M6/p1/p1/fizz_buzz.py
+++ M6/p1/p1/fizz_buzz.py
@@ -8,7 +8,17 @@ def main():
     '''
     Read number from the input, store it in variable num.
     '''
-    num = int(input())
-
+NUM = int(input())
+i = 0
+for i in range(NUM):
+    if i%3 == 0 and i%5 == 0:
+        print("fizzbuzz")
+    elif i%3 == 0:
+        print("fizz")
+    elif i%5 == 0:
+        print("buzz")
+    else:
+        print(i)
+    i = i+1
 if __name__ == "__main__":
     main()
