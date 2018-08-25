447710ec6a0cba3fda144840e339340c574669db
KusumaNavya <navya.kusuma21@msitprogram.net>
2018-08-25 13:10:52 +0530

    " -> 0 of 4 passed. pylint: 5.79/10 "

--- M22/assignment3/tokenize.py
+++ M22/assignment3/tokenize.py
@@ -3,36 +3,24 @@ Write a function to tokenize a given string and return a dictionary with the fre
 each word
 '''
 import re
+def tokenize(string):
+    regex = re.compile("[^0-9 A-Z a-z]")
+    t = {}
+    l = []
+    for line in string:
+        l.append(regex.sub("",line).split())
 
-def word_count(string):
-    my_string = string.lower().split()
-    my_dict = {}
-    for item in my_string:
-        if item in my_dict:
-            my_dict[item] += 1
-        else:
-            my_dict[item] = 1
-    print(my_dict)
-
-# def tokenize(string):
-#     regex = re.compile("[^0-9 A-Z a-z]")
-#     t = {}
-#     l = []
-#     for line in string:
-#         l.append(regex.sub("",line).split())
-
-#     for s in l:
-#         for w in s:
-#             t[s] = t.get(w,0) + 1
-#     return t
+    for s in l:
+        for w in s:
+            t[s] = t.get(w) + 1
+    return t
 
 def main():
     n = int(input())
     lst = []
     for _ in range(n):
         lst.append(input())
-    # print(tokenize(lst))
-    print(word_count(string))
+    print(tokenize(lst))
 
 if __name__ == '__main__':
     main()
