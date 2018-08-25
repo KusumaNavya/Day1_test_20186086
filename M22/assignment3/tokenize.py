1cf55c538d01b6035431b47faea904a9b71d3c1e
KusumaNavya <navya.kusuma21@msitprogram.net>
2018-08-25 12:37:19 +0530

    " -> 0 of 4 passed. pylint: -0.53/10 "

--- M22/assignment3/tokenize.py
+++ M22/assignment3/tokenize.py
@@ -4,22 +4,22 @@ each word
 '''
 import re
 def tokenize(string):
-    regex = re.compile("[^A-Z a-z 0-9]")
+    regex = re.compile("[^0-9 A-Z a-z]")
     t = {}
     l = []
     for line in string:
-    	l.append(reg.sub('',line).split())
+        l.append(reg.sub('',line).split())
 
     for s in l:
-    	for w in s:
-    		t[s] = t.get(w,0) +
+        for w in s:
+            t[s] = t.get(w,0) + 1
     return t
 
 def main():
     n = int(imput())
     lst = []
     for _ in range(n):
-    	lst.append(input())
+        lst.append(input())
     print(tokenize(lst))
 
 if __name__ == '__main__':
